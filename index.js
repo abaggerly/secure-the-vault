54e0c8da720b5b47a4d6757e49465c6209b6b254
Amador Baggerly <amador.baggerly@gmail.com>
2025-07-23 11:09:24 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = 70 / 2 + 5;
+
+let code3 = (25 % 9) + (15 % 8) + 25;
+
+const message = "The vault has been secured. The combination is: ";
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+let codeB= `${code1}-${code2}-${code3}`;
+
+console.log(message+codeA);
+
+console.log(message+codeB);
